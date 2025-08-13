88df215467d8b8b3e19a46180b91af25938240b0
jcaraujojc <joaocarlos3b2020@hotmail.com>
2025-08-13 20:12:01 -0300

    Pratica

--- APPAula/App.js
+++ APPAula/App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}> 
+        <UserProfile 
+          nome="João Araújo" 
+          profissao="Desenvolvedor Junior" 
+          descricao="Formado em 12/2025" 
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+class UserProfile extends Component {
+  render() {
+    const { nome, profissao, descricao } = this.props;
+
+    return (
+      <View style={styles.card}>
+         <Text style={{ fontSize: 25, color: 'black', marginBottom: 80, textAlign: 'center' }}>
+          Cartão De Perfil De Usuário
+        </Text>
+        <Text style={styles.text}>Nome: {nome}</Text>
+        <Text style={styles.text}>Profissão: {profissao}</Text>
+        <Text style={styles.text}>Descrição: {descricao}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +41,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    backgroundColor: '#9ca9f5ff',
+    padding: 20,
+    borderRadius: 30,
+    width: '80%',
+    alignItems: 'flex-start',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 5,
+    color: 'black',
+  },
 });
+
+export default App;
